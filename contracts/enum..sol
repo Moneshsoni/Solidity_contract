fe95a4e7f99ec578bafe350a24eb97af239ce7c9
Moneshsoni <sonimonesh115@gmail.com>
2022-10-02 18:00:13 +0530

    make enum contract

---
+++ contracts/enum..sol
@@ -0,0 +1,32 @@
+pragma solidity 0.8.9;
+
+contract Enum {
+    enum Status {
+        Pending,
+        Shiped,
+        Accepted,
+        Rejected,
+        Cancaled
+    }
+
+    Status public status;
+
+    //get function for getting value
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    //set enum value
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    //cancal then
+    function cancal() public {
+        status = Status.Cancaled;
+    }
+
+    function remove_status()public{
+        delete status;
+    }
+}
