ab2696a82d71e1ba0544e8845621952a8d183f55
Moneshsoni <sonimonesh115@gmail.com>
2022-10-05 01:18:39 +0530

    add external data

--- contracts/libadd.sol
+++ contracts/libadd.sol
@@ -1,17 +1,26 @@
 pragma solidity 0.8.9;
-contract Libadd{
-    uint public a;
-    uint public b;
-    function add(uint _a, uint _b) external{
-        _a=a;
-        _b=b;
+
+contract Libadd {
+    uint256 public a;
+    uint256 public b;
+
+    function add(uint256 _a, uint256 _b) external {
+        a = _a;
+        b = _b;
+    }
+
+    function get_add() public view returns (uint256) {
+        return a + b;
     }
     
-    function getadd()public view returns(uint){
-        return a+b;
-    }
+    // function get_add1()public{
+    //     add(12,12);
+    // }
 }
 
-contract A is Libadd{
+// contract Lib1{
+//     function (uint _a)e
+// }
+contract A is Libadd {
     
 }
