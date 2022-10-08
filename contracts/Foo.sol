9edd1e5db2726bb67cc16fe5c24638febb8eee38
Moneshsoni <sonimonesh115@gmail.com>
2022-10-08 22:00:20 +0530

    SS IMPORT ANND TRY CATCH CONTRACT

---
+++ contracts/Foo.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.8.9;
+struct Point{
+    uint x;
+    uint y;
+}
+
+error Unauthorized(address caller);
+function add(uint x,uint y)pure returns(uint){
+    return x+y;
+}
+
+contract Foo{
+    string public name = "Foo";
+}
