920bd762ea6beb4e041bc14a172e0085e032f444
Moneshsoni <sonimonesh115@gmail.com>
2022-10-09 17:38:34 +0530

    add access modifier

--- contracts/For_loop.sol
+++ contracts/For_loop.sol
@@ -1,5 +1,4 @@
 pragma solidity 0.8.9;
-
 contract Loops {
     function loops() external pure {
         for (uint256 i = 0; i < 10; i++) {
@@ -17,15 +16,28 @@ contract Loops {
         }
     }
 
-    function sum(uint256 _n) external pure returns (uint256) {
-        uint256 s;
-        for (uint256 i = 1; i <= _n; i++) {
-            s += i;
+    modifier limit(){
+        require(_num>5, "please enter greater than 5 value");
+        _;
+    }
+    
+    
+    function sum(uint256 _n) external pure limit returns (uint256) {
+        if (_n < 5) {
+            revert("value is less than 5");
+        } else {
+            uint256 s;
+            for (uint256 i = 1; i <= _n; i++) {
+                s += i;
+            }
+            return s;
         }
-        return s;
+
+        
     }
 
     function sqrt_sum(uint256 _num) public pure returns (uint256) {
+       
         uint256 g;
         for (uint256 j = 1; j <= _num; j++) {
             g += j;
