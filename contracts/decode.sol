aa70e531f38a4ff45a684c96bd185a89bc8e1d9c
Moneshsoni <sonimonesh115@gmail.com>
2022-10-12 00:32:03 +0530

    add decode file

---
+++ contracts/decode.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.13;
+
+contract AbiDecode {
+    struct MyStruct {
+        string name;
+        uint256[2] nums;
+    }
+
+    function encode(
+        uint256 x,
+        address addr,
+        uint[] calldata arr,
+        uint256[] calldata myStruct
+    ) external pure returns (bytes memory) {
+        return abi.encode(x, addr, arr, myStruct);
+    }
+    
+    
+    function decode(bytes calldata data)
+        external pure returns(uint x, address addr,
+        uint[] memory arr,
+        MyStruct memory myStruct){
+        (x,addr,arr,myStruct)=abi.decode(data,(uint, address, uint[],MyStruct));
+    }
+}
