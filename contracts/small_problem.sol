00ec3d3536c55c39fd8e02f5337f3779e3a7413d
Moneshsoni <sonimonesh115@gmail.com>
2022-10-12 10:11:44 +0530

    add keccak 256

--- contracts/small_problem.sol
+++ contracts/small_problem.sol
@@ -1,15 +1,15 @@
 pragma solidity 0.8.13;
-contract Learn{
 
-    uint public num1=5;
-    uint public num2=10;
-    uint public product;
+contract Learn {
+    uint256 public num1 = 5;
+    uint256 public num2 = 10;
+    uint256 public product;
 
-    function call_state()public {
-        product = num1*num2;
+    function call_state() public {
+        product = num1 * num2;
     }
 
-    function get_value()public view returns(uint){
+    function get_value() public view returns (uint256) {
         return product;
     }
 }
